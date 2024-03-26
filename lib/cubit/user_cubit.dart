18355a89465a931f4f7192762a49d07c61bb26b7
mohamed <mezoattalla123@gmail.com>
2024-03-26 03:08:12 +0200

    Refactor user state classes and cubit to handle sign-in functionality with Dio for API calls. Update sign-in screen UI to display loading indicator and handle success/failure states accordingly. Add Dio dependency for HTTP requests in pubspec.yaml.

--- lib/cubit/user_cubit.dart
+++ lib/cubit/user_cubit.dart
@@ -1,3 +1,4 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:happy_tech_mastering_api_with_flutter/cubit/user_state.dart';
@@ -5,24 +6,52 @@ import 'package:image_picker/image_picker.dart';
 
 class UserCubit extends Cubit<UserState> {
   UserCubit() : super(UserInitial());
+
   //Sign in Form key
   GlobalKey<FormState> signInFormKey = GlobalKey();
+
   //Sign in email
   TextEditingController signInEmail = TextEditingController();
+
   //Sign in password
   TextEditingController signInPassword = TextEditingController();
+
   //Sign Up Form key
   GlobalKey<FormState> signUpFormKey = GlobalKey();
+
   //Profile Pic
   XFile? profilePic;
+
   //Sign up name
   TextEditingController signUpName = TextEditingController();
+
   //Sign up phone number
   TextEditingController signUpPhoneNumber = TextEditingController();
+
   //Sign up email
   TextEditingController signUpEmail = TextEditingController();
+
   //Sign up password
   TextEditingController signUpPassword = TextEditingController();
+
   //Sign up confirm password
   TextEditingController confirmPassword = TextEditingController();
+
+  signIn() async {
+    emit(SignInLoading());
+    try {
+      final response = await Dio().post(
+        "https://food-api-omega.vercel.app/api/v1/user/signin",
+        data: {
+          "email": signInEmail.text,
+          "password": signInPassword.text,
+        },
+      );
+      emit(SignInSuccess());
+      print(response);
+    } catch (e) {
+      emit(SignInFailure(error: e.toString()));
+      print(e.toString());
+    }
+  }
 }
