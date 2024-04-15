e29e11dc6e748e16d6ae086ed2968d7d37c6cfa9
mohamed <mezoattalla123@gmail.com>
2024-04-15 07:44:47 +0200

    Added ApiInterceptor class to handle API requests with Accept-Language header set to 'en'.

---
+++ lib/core/api/api_interceptor.dart
@@ -0,0 +1,9 @@
+import 'package:dio/dio.dart';
+
+class ApiInterceptor extends Interceptor {
+  @override
+  void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
+    super.onRequest(options, handler);
+    options.headers['Accept-Language'] = 'en';
+  }
+}
