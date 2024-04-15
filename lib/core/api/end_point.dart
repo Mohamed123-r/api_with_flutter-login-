1f77013e93e149e1c80404c94f1c84795fab26ff
mohamed <mezoattalla123@gmail.com>
2024-04-15 07:44:54 +0200

    Added ApiInterceptor class to handle API requests with Accept-Language header set to 'en'.

---
+++ lib/core/api/end_point.dart
@@ -0,0 +1,10 @@
+class EndPoint {
+  static String baseUrl = "https://food-api-omega.vercel.app/api/v1/";
+  static String signIn = "user/signin";
+}
+
+class ApiKeys {
+  static String token = "token";
+  static String status = "status";
+  static String errorMessage = "ErrorMessage";
+}
